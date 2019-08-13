19594735c0e8152e7a122484137921173b4260f2
Tosuke <tasukeprg@gmail.com>
2019-08-13 14:27:02 +0900

    add relativetime

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,64 @@ export function format(strings: TemplateStringsArray, ...values: Array<string | 
   }
 }
 
-export const yyyy: DateFormatter = date => `${date.getFullYear()}`.padStart(4, '0')
-export const MM: DateFormatter = date => `${date.getMonth() + 1}`.padStart(2, '0')
-export const dd: DateFormatter = date => `${date.getDate()}`.padStart(2, '0')
+// 0方向丸め
+const fix = (num: number) => num > 0 ? Math.floor(num) : Math.ceil(num)
+
+const SECOND_MS = 1000
+const MINUTE_MS = 60 * SECOND_MS
+const HOUR_MS = 60 * MINUTE_MS
+const DAY_MS = 24 * HOUR_MS
+const WEEK_MS = 7 * DAY_MS
+
+export function formatRelativeTime(
+  formatters: Partial<{
+    year: (yearDiff: number, left: Date, right: Date) => string
+    month: (monthDiff: number, left: Date, right: Date) => string
+    week: (weekDiff: number, left: Date, right: Date) => string
+    date: (dateDiff: number, left: Date, right: Date) => string
+    hours: (hoursDiff: number, left: Date, right: Date) => string
+    minutes: (minutesDiff: number, left: Date, right: Date) => string
+    seconds: (secondsDiff: number, left: Date, right: Date) => string
+  }>,
+  left: Date,
+  right: Date,
+): string {
+  const diff = left.getTime() - right.getTime()
+  if(Math.abs(diff) >= DAY_MS * 31) {
+    const yearDiff = left.getFullYear() - right.getFullYear()
+    if(Math.abs(yearDiff) > 0 && formatters.year) {
+      return formatters.year(yearDiff, left, right)
+    } else if(formatters.month) {
+      return formatters.month((left.getMonth() - right.getMonth()) + yearDiff * 12, left, right)
+    }
+  } else if(Math.abs(diff) >= WEEK_MS && formatters.week) {
+    return formatters.week(fix(diff / WEEK_MS), left, right)
+  } else if(Math.abs(diff) >= DAY_MS && formatters.date) {
+    return formatters.date(fix(diff / DAY_MS),left, right)
+  } else if(Math.abs(diff) >= HOUR_MS && formatters.hours) {
+    return formatters.hours(fix(diff / HOUR_MS), left, right)
+  } else if(Math.abs(diff) >= MINUTE_MS && formatters.minutes) {
+    return formatters.minutes(fix(diff / MINUTE_MS), left, right)
+  } else if(formatters.seconds) {
+    return formatters.seconds(fix(diff / SECOND_MS), left, right)
+  } else {
+    return `${diff}ms`
+  }
+  throw 'unreachable'
+}
+
+function padZero(num: number, maxLength: number) {
+  return `${num}`.padStart(maxLength, '0')
+}
+
+export const yyyy: DateFormatter = date => padZero(date.getFullYear(), 4)
+
+export const MM: DateFormatter = date => padZero(date.getMonth() + 1, 2)
+
+export const dd: DateFormatter = date => padZero(date.getDate(), 2)
+
+export const HH: DateFormatter = date => padZero(date.getHours(), 2)
+
+export const mm: DateFormatter = date => padZero(date.getMinutes(), 2)
+
+export const ss: DateFormatter = date => padZero(date.getSeconds(), 2)
--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,13 @@
-import { format, yyyy, MM, dd } from '.'
+import { format, yyyy, MM, dd, HH, mm, ss, formatRelativeTime } from '.'
 
-console.log(format`${yyyy}-${MM}-${dd}`(new Date()))
+console.log(format`${yyyy}-${MM}-${dd}T${HH}:${mm}:${ss}`(new Date())) // --> 2019-08-12
+
+console.log(formatRelativeTime({
+  year: n => `${n} year(s) ago`,
+  month: n => `${n} month(s) ago`,
+  week: n => `${n} week(s) ago`,
+  date: n => `${n} day(s) ago`,
+  hours: n => `${n} hour(s) ago`,
+  // minutes: n => `${n} minute(s) ago`,
+  seconds: n => n < 5 ? 'just now' : `${n} second(s) ago`
+}, new Date(), new Date(2019, 7, 13, 14, 11)))
